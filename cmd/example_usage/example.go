61c1292a3409ac7b539044d2fe6dc161a195011b
Jack <Jack-0@users.noreply.github.com>
2025-06-30 13:09:40 +0100

    docs: added basic documentation and example
    added readme.md
    added launch.json for vscode debugging
    added an example

---
+++ cmd/example_usage/example.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jack-0/go-logpoint/logpoint"
+)
+
+func main() {
+	logpointURL := "https://<required>"
+	logpointUsername := "<required>"
+	logpointUserSecret := "<required>"
+
+	if logpointUserSecret == "<required>" || logpointURL == "https://<required>" || logpointUsername == "<required>" {
+		panic("Ensure you alter all required variables")
+	}
+
+	lp := logpoint.New(logpointURL, logpointUsername, logpointUserSecret)
+	a, err := lp.GetRepos()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("📃 Allowed Repos for given user:")
+	fmt.Println(a.AllowedRepos)
+
+	allRepos := []string{}
+	for _, repo := range a.AllowedRepos {
+		allRepos = append(allRepos, repo.Repo)
+	}
+
+	fmt.Println("🔍 Query 100 Logs from those repos in the last hour:")
+	b, err := lp.Query("", "Last 1 hour", 100, allRepos, 10)
+	if err != nil {
+		panic(err)
+	}
+
+	qr, err := lp.QueryResult(b.SearchId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//// Uncomment the following to see results
+	fmt.Println("...Uncomment example code to see full results...")
+	// fmt.Println("🎁 Query Results:")
+	// for _, item := range qr {
+	// 	fmt.Println(item)
+	// }
+
+	fmt.Println("🏁 Total items:")
+	fmt.Println(len(qr))
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Go Logpoint
+
+A very simple wrapper library for the Logpoint API.
+
+
+## Usage
+See the example at [./cmd/example_usage/example.go](./cmd/example_usage/example.go)
